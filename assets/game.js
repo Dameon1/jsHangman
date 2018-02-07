8f0d79aab56b8c59c1383bacab2a44ecb5b9e56d
Dameon1 <dameonmedoza@gmail.com>
2018-02-07 08:56:07 -0700

    added pseudo code
    a little baseline to start working from.

--- assets/game.js
+++ assets/game.js
@@ -11,7 +11,30 @@
 let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 
 
+let numbGuess;
+let wins;
+let loses;
+let randNumb;
+let letsWord;
+let letGuessed;
+let wrongGuess;
 
+let reset = function(){
+    // resets all gameplay to zero ,. not wins and losses those are preset
+}
 
+let checked = function(letter){
+    // create boolean
+    // for loop to change if letter is in word to true
+    // if true , change letter by giving it a new value
+    // if false, push letter into array and change guesses left
+}
 
+let winOrLose = function(){
+    // change HTML
+    // if word === an array . toString . change Html and reset
+    // if guesses === 0 , html reset
+}
 
+// onClick functions for letters
+// call game functionality with Document.onReady
